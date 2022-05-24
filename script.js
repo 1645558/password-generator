1df74981fabde711d67bcad6f0e9b32e30ed62b0
Rare Raptor <connorwtuncc@gmail.com>
2022-05-24 14:25:52 -0400

    simplified the if statements

--- script.js
+++ script.js
@@ -1,78 +1,66 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-//Instructor Provided Template: Anthony Cooper
-function generatePassword() {
   var lowercase = "abcdefghjklmnopqrstuvwxyz"
   var numbers = "123456789"
   var special = "#$%&()*+,-./:;<=>?"
   var uppercase = lowercase.toUpperCase();
-
+  var choices = ''
+//Instructor Provided Template: Anthony Cooper
+function generatePassword() {
+  
   var input = Number(prompt("Choose a length between 8 and 128 characters"));
-
+  
   if (input >= 8 && input <= 128) {
     //GOOD CODE GOES HERE
-    console.log(input)
+    console.log(input);
   } else {
-    alert('Please enter a valid amount of characters')
+    alert('Please enter a valid amount of characters');
   }
 
-  var lowercase = prompt('Do you want to include lowercase letters?');
+  var lowercase = confirm('Do you want to include lowercase letters?');
 
-  if (lowercase === 'yes') {
-    alert('Sounds good!');
-  } else if (lowercase === 'no') {
-    alert('No problem!')
+  if (lowercase) {
+    choices += lowercase
   }
   console.log(lowercase);
   
-  var uppercase = prompt('Do you want to include any uppercase letters?')
-
-  if (uppercase === 'yes') {
-    alert('Awesome!')
-  } else if (uppercase === 'no') {
-    alert('No worries!')
+  var uppercase = confirm('Do you want to include any uppercase letters?')
+  
+  if (uppercase) {
+    choices += uppercase
   }
   console.log(uppercase)
-
-  var numbers = prompt('Do you want to include numbers?')
-
-  if (numbers === 'yes') {
-    alert('Sweet!')
-  } else if (numbers === 'no') {
-    alert('Unfortunate')
-  }
-
-  var special = prompt('Do you want to include any special characters?')
-
-  if (special === 'yes') {
-    alert('Makes it strong!')
-  } else if (special === 'no') {
-    alert('Nooooo pls noooo')
-  }
-
-  return "";
-
   
+  var numbers = confirm('Do you want to include numbers?')
+  
+  if (numbers) {
+    choices += numbers
+  }
+  console.log(numbers)
+  
+  var special = confirm('Do you want to include any special characters?')
+  
+  if (special) {
+    choices += special
+  }
+  console.log(special)
+
+  var newPassword = ''
+  for (var i = 0; i < input; i++) {
+    let choices = (Math.floor(Math.random) * lowercase.length)
+  }
+
+  return newPassword;
+
 }
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(lowercase, numbers, special, uppercase) {
+  var password = generatePassword()
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
-  
-
 
 
 }
-
-
-
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
